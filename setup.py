4c0ed3234ec6faaa3102e61d72c6635f899d31c0
Michelangelo Conserva <mi.conserva@gmail.com>
2022-10-23 11:20:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,76 +1,56 @@
-import os
-import pathlib
-
+import imp
 import setuptools
 
-HERE = pathlib.Path(__file__).parent
-
-VERSION = "1.2.0"
-DESCRIPTION = "Research-oriented library for reinforcement learning."
-LICENSE = "MIT License"
-PACKAGE_NAME = "colosseum"
-KEYWORDS = "reinforcement-learning python"
-
-AUTHOR = "Anonymous"
-AUTHOR_EMAIL = "Anonymous"
-URL = "Anonymous"
-
-libraries = []
-if os.name == "posix":
-    libraries.append("m")
-
-
 setuptools.setup(
-    name=PACKAGE_NAME,
-    description=DESCRIPTION,
-    long_description=(HERE / "README.md").read_text(),
+    name="rl-colosseum",
+    description=(
+        "A pioneering Python package that creates a bridge between theory and practice in tabular reinforcement learning."
+    ),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="Anonymous",
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    license=LICENSE,
-    version=VERSION,
-    keywords=KEYWORDS,
+    url="https://github.com/MichelangeloConserva/Colosseum",
+    author="Michelangelo Conserva",
+    author_email="mi.conserva@gmail.com",
+    license="Apache License, Version 2.0",
+    version=imp.load_source("_metadata", "colosseum/_metadata.py").__version__,
+    keywords="reinforcement-learning python machine-learning",
     packages=setuptools.find_packages(),
     install_requires=[
         "adjustText",
-        "bsuite",
         "dm-env",
-        "dm-sonnet",
-        "frozendict",
         "gin-config",
         "gym",
         "matplotlib",
         "networkx",
         "numpy",
         "pandas",
-        "pebble",
-        "pygraphviz",
         "pydtmc",
         "pygame",
         "pyyaml",
-        "ray[tune]==1.9.1",
+        "ray[tune]",
         "scipy>=1.7.3",
-        "seaborn>=0.12",
+        "seaborn>=0.10",
         "sparse",
-        "tensorflow",
-        "timeout-decorator",
         "toolz",
         "tqdm",
         "PyYAML>=6.0",
-        "wrapt_timeout_decorator",
+        "pygraphviz",
+        "wrapt-timeout-decorator",
+        "bsuite",
+        "dm-sonnet",
+        "tensorflow",
+        "tensorflow-probability"
     ],
-    # scripts=glob("bin" + os.sep + "*"),
     classifiers=[
         "Development Status :: 3 - Alpha",
+        "Environment :: Console",
         "Intended Audience :: Science/Research",
-        # 'License :: OSI Approved :: Apache Software License',
+        "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
-        # 'Operating System :: Microsoft :: Windows',
-        # 'Operating System :: MacOS :: MacOS X',
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
-    # zip_safe=F,  # Doesn't create an egg - easier to debug and hack on
 )
-
